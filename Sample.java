263f54589ff4d9d35c3d7e45cb4dd0af10cef25e
Thomas <ricketta@miamioh.edu>
2015-04-28 13:38:46 -0400

    Added 2 new queries. Only one more to go.

--- Sample.java
+++ Sample.java
@@ -19,20 +19,44 @@ public class Sample
     try
     {
       // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:Pokemon.db");
+      connection = DriverManager.getConnection("jdbc:sqlite:PokeData.db");
       Statement statement = connection.createStatement();
       statement.setQueryTimeout(30);  // set timeout to 30 sec.
 	
-		System.out.println("Would you like to see a list of all Pokemon? (Yes/No)");
-		String yesNo = scan.next();
-		if(yesNo.toLowerCase().equals("yes")) {
+		System.out.println("1. Count how many types are super effective against Fire");
+		System.out.println("2. Update trainer team 1 with Ivysaur and Venusaur");
+		System.out.println("3. Req 3...");
+		System.out.println("4. Show all Pokemon.");
+		String ans = scan.next();
+		if(ans.equals("4")) {
       ResultSet rs = statement.executeQuery("SELECT name FROM pokemon");
 		int count = 0;
 	  while(rs.next())
       {
         // read the result set
-        System.out.println("Pokemon" + count + " is " + rs.getString("name"));
+        System.out.println("Pokemon " + (count+1) + " is " + rs.getString("name"));
+		count++;
       }
+	  }
+	  
+	  else if(ans.equals("3")){
+		  
+	  }
+	  
+	  else if(ans.equals("2")){
+		  ResultSet rs = statement.executeQuery("UPDATE trainer_team SET p1_id = 002," +
+		  " p2_id = 002, p3_id = 002, p4_id = 002, p5_id = 002, p6_id = 003 WHERE " + 
+		  "trainer_id = 1;");
+	  }
+	  
+	  else if(ans.equals("1")){
+		  ResultSet rs = statement.executeQuery("select count(*) from " +
+		  "effectiveness where strength like \'%fire%\';");
+		  System.out.println(rs.getString("count(*)"));
+	  }
+	  
+	  else{
+		  System.out.println("Not a valid option for prototype. Shutting down.");
 	  }
 
     }
