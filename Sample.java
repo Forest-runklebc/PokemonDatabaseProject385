535b60a24faf9137cd2591cdd2eabc8600b3ac01
Thomas Ricketts <ricketta@miamioh.edu>
2015-05-02 12:05:06 -0400

    Wrote over prototype. Working on making a section of code that asks if the user is new, or is a previous visitor.

--- Sample.java
+++ Sample.java
@@ -22,51 +22,34 @@ public class Sample
       connection = DriverManager.getConnection("jdbc:sqlite:PokeData.db");
       Statement statement = connection.createStatement();
       statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-      System.out.println("1. Count how many types are super effective against Fire");
-      System.out.println("2. Update trainer team 1 with Ivysaur and Venusaur");
-      System.out.println("3. Show all Pokemon Strengths and Weaknesses");
-      System.out.println("4. Show all Pokemon.");
-      System.out.print("Please input your choice (1-4): ");
-      String ans = scan.next();
-      if(ans.equals("1")){
-        ResultSet rs = statement.executeQuery("select count(*) from " +
-            "effectiveness where strength like \'%fire%\';");
-        System.out.println(rs.getString("count(*)"));
-      }
-      else if(ans.equals("2")){
-        ResultSet rs = statement.executeQuery("UPDATE trainer_team SET p1_id = 002," +
-            " p2_id = 002, p3_id = 002, p4_id = 002, p5_id = 002, p6_id = 003 WHERE " + 
-            "trainer_id = 1;");
-      }
-      else if(ans.equals("3")){
-        ResultSet rs = statement.executeQuery("SELECT name, strength, weakness " +
-            "FROM pokemon as P join effectiveness as E on P.type = E.base_type;");
-        String poke = "";
-        String stren = "";
-        String weak = "";
-        while(rs.next())
-        {
-          poke = rs.getString("name");
-          stren = rs.getString("strength");
-          weak = rs.getString("weakness");
-          System.out.println(poke + " is strong against " + stren + " and is " +
-              "weak against " + weak);
-        }
-      }
-      else if(ans.equals("4")) {
-        ResultSet rs = statement.executeQuery("SELECT name FROM pokemon");
-        int count = 0;
-        while(rs.next())
-        {
-          System.out.println("Pokemon " + (count+1) + " is " + rs.getString("name"));
-          count++;
-        }
-      }
-      else{
-        System.out.println("Not a valid option for prototype. Shutting down.");
-      }
-
+	  String trainerID = "";
+	  String ans = "";
+	  do{
+      System.out.println("Welcome to Pokemon Squad Analyzer! Are you new here?(y/n)");
+      ans = scan.next();
+      if(ans.equalsIgnoreCase("y")){
+		  String gender = "";
+		  do{
+			  System.out.println("Are you a boy or a girl?");
+			  gender = scan.next();
+		  } while(!gender.equalsIgnoreCase("boy") && !gender.equalsIgnoreCase("girl"));
+		  if(gender.equalsIgnoreCase("boy")){
+			  gender = "male";
+		  } else{
+			  gender = "female";
+		  }
+		  String name = "";
+		  System.out.println("What it your name? MAX 25 CHARS");
+		  name = scan.nextLine();
+		  name = scan.nextLine();
+		  String pNum = "";
+		  do{
+			  System.out.println("Enter only the numbers that compose your " +
+			  "phone number: 10 DIGITS");
+			  pNum = scan.next();
+		  } while(pNum.length() > 10);
+	  }
+	  } while(!ans.equalsIgnoreCase("y") && !ans.equalsIgnoreCase("n"));
     }
     catch(SQLException e)
     {
